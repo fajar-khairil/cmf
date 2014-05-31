0b33cb3306c2e48f68f81b929236e4fe0d552a34
fajar <fajar90alone@gmail.com>
2014-05-31 20:26:56 +0000

    restructuring directory

--- commit.sh
+++ commit.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 #
 # use this script while commit
-# removing garbage on engine/tmp
+# removing garbage on tmp
 #
 
 echo "removing garbage"
-rm -rf engine/tmp/cache/*
-rm -rf engine/tmp/sessions/*
-rm -rf engine/tmp/views/*
-rm -rf engine/tmp/profiler/*
-rm -rf engine/logs/profiler/*
+rm -rf tmp/cache/*
+rm -rf tmp/sessions/*
+rm -rf tmp/views/*
+rm -rf tmp/profiler/*
+rm -rf tmp/logs/*
 
 echo "keep several directory"
-touch engine/tmp/cache/.gitkeep
-touch engine/tmp/sessions/.gitkeep
-touch engine/tmp/views/.gitkeep
-touch engine/tmp/profiler/.gitkeep
-touch engine/tmp/logs/.gitkeep
+touch tmp/cache/.gitkeep
+touch tmp/sessions/.gitkeep
+touch tmp/views/.gitkeep
+touch tmp/profiler/.gitkeep
+touch tmp/logs/.gitkeep
 
 git add -A && git commit
 echo "done"
