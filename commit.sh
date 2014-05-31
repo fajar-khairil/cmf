dc543fdff7dca37b5be767c96597916ef33cc2ed
fajar <fajar90alone@gmail.com>
2014-05-31 08:04:24 +0000

    commit script helper

---
+++ commit.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# use this script while commit
+# removing garbage on engine/tmp
+#
+
+echo "removing garbage"
+rm -rf engine/tmp/cache/*
+rm -rf engine/tmp/sessions/*
+rm -rf engine/tmp/views/*
+rm -rf engine/tmp/profiler/*
+
+echo "keep several directory"
+touch engine/tmp/cache/.gitkeep
+touch engine/tmp/sessions/.gitkeep
+touch engine/tmp/views/.gitkeep
+touch engine/tmp/profiler/.gitkeep
+
+git add -A && git commit
+echo "done"
+exit 0
