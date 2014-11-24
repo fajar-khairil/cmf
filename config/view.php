1207c9644e9c0bc760f50dace76a7bcf754a4366
fajar <fajar90alone@gmail.com>
2014-11-24 17:52:05 +0000

    cleaning

--- config/view.php
+++
@@ -1,8 +0,0 @@
-<?php
-return array(
-	'blade'	=> array(
-		'cache'		=>	Application::$ENGINE_PATH.DIRECTORY_SEPARATOR.'tmp'.DIRECTORY_SEPARATOR.'views'.DIRECTORY_SEPARATOR.'blade',
-		'content_tags'	=>	['[[',']]'],
-		'escaped_content_tags'	=>	['[[[',']]]']
-	)
-);
--- public/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-	require_once realpath('../bootstrap.php');
-?>
