24e5a7c302b78969bb5acf45b1053e7c74dcda2a
fajar <fajar90alone@gmail.com>
2014-11-29 19:47:45 +0000

    initial commit

---
+++ commit.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# use this script while commit
+# removing garbage on tmp folder
+#
+
+echo "removing garbage files.."
+rm -rf tmp/cache/*
+rm -rf tmp/sessions/*
+rm -rf tmp/views/*
+rm -rf tmp/logs/*
+
+echo "keep temporary diretories.."
+touch tmp/cache/.gitkeep
+touch tmp/sessions/.gitkeep
+touch tmp/views/.gitkeep
+touch tmp/logs/.gitkeep
+
+if [ "$1" == "commit" ] 
+then
+  echo "committing..."
+  git add -A && git commit
+fi
+
+echo "all done :-)"
+exit 0
