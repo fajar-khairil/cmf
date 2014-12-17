3fdcdeb1fb3bca2278296b4a090f412e03654267
fajar <fajar90alone@gmail.com>
2014-12-17 05:23:14 +0000

    more complete CacheManager testCase

--- tests/Unika/Tests/Ext/CacheManagerTest.php
+++ tests/Unika/Tests/Ext/CacheManagerTest.php
@@ -15,6 +15,7 @@ class CacheMangerTest extends AppTestCase
     protected function setUp()
     {
     	parent::setUp();
+    	$this->app['config']['cache.driver'] = 'Apc';
     	$this->cacheManager = $this->app['cache.manager'];
     }
 
@@ -25,14 +26,26 @@ class CacheMangerTest extends AppTestCase
 
     public function testGetDefaultDriver()
     {
-  		$this->assertEquals( 'File' , $this->cacheManager->getDefaultDriver() );
+  		$this->assertEquals( 'Apc' , $this->cacheManager->getDefaultDriver() );
     }
 
     public function testGetDriver()
     {
-        // Stop here and mark this test as incomplete.
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );    	
+        $this->assertTrue( $this->cacheManager->driver() instanceof Illuminate\Cache\ApcStore );  
+        $this->assertTrue( $this->cacheManager->driver('File') instanceof Illuminate\Cache\FileStore ); 
+        $this->assertTrue( $this->cacheManager->driver('Apc') instanceof Illuminate\Cache\ApcStore );
+        $this->assertTrue( $this->cacheManager->driver('Array') instanceof Illuminate\Cache\ArrayStore );
+        $this->assertTrue( $this->cacheManager->driver('Null') instanceof Illuminate\Cache\NullStore );
+        $this->assertTrue( $this->cacheManager->driver('Wincache') instanceof Illuminate\Cache\WincacheStore );
+        $this->assertTrue( $this->cacheManager->driver('Xcache') instanceof Illuminate\Cache\XcacheStore );
+        $this->assertTrue( $this->cacheManager->driver('Memcached') instanceof Illuminate\Cache\MemcachedStore );
+
+        if( class_exists('\Illuminate\Redis\Database') )
+        	$this->assertTrue( $this->cacheManager->driver('Redis') instanceof Illuminate\Cache\RedisStore );
+    }
+
+    public function testCacheProvider()
+    {
+    	$this->assertTrue( $this->app['cache'] instanceof \Illuminate\Cache\Repository );
     }
 }
